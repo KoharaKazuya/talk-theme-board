a9a78c285d8c6eba22b37d7071c0f360071e5707
KoharaKazuya <reflect.results@gmail.com>
2022-02-19 12:42:10 +0900

    feat: ヘルプ機能を追加する

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,16 +6,25 @@
 mod parser;
 
 use crate::parser::{load_file, load_text};
-use tauri::{Menu, MenuItem, Submenu};
+use tauri::{api::dialog, CustomMenuItem, Menu, MenuItem, Submenu};
+
+const CHEAT_SHEET: &str = "Cmd + Shift + o: クリップボードから開く\nCmd + Shift + j: 次のテーマ\nCmd + Shift + k: 前のテーマ\nCmd + Shift + Control + c: 終了する";
 
 fn main() {
   let menu = Menu::new().add_submenu(Submenu::new(
     "app",
-    Menu::new().add_native_item(MenuItem::Quit),
+    Menu::new()
+      .add_item(CustomMenuItem::new("help", "ヘルプ"))
+      .add_native_item(MenuItem::Quit),
   ));
 
   tauri::Builder::default()
     .menu(menu)
+    .on_menu_event(|event| {
+      if event.menu_item_id() == "help" {
+        dialog::message(Some(event.window()), "ヘルプ", CHEAT_SHEET);
+      }
+    })
     .invoke_handler(tauri::generate_handler![load_text, load_file])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
